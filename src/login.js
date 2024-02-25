8a3e3e6313a02d2d8550ed331729e70c47d81340
Akshaya-1002 <147040183+Akshaya-1002@users.noreply.github.com>
2024-02-25 21:12:31 +0530

    HuluApp

---
+++ src/login.js
@@ -0,0 +1,63 @@
+// import React from 'react'
+
+// const Login = () => {
+//   return (
+        // const modal = document.querySelector('.modal')
+        // const loginBtn = document.querySelector('.login-btn')
+        // const closeBtn = document.querySelector('.close')
+
+        // loginBtn.addEventListener('click', openModal)
+        // closeBtn.addEventListener('click', closeModal)
+        // window.addEventListener('click', outsideClick)
+
+        // function openModal() {
+        //     modal.style.display = 'block'
+        // }
+
+        // function closeModal() {
+        //     modal.style.display = 'none'
+        // }
+
+        // function outsideClick(e) {
+        //     if (e.target == modal) {
+        //         closeModal()
+        //     }
+        // }
+//     <div>
+    //   <header className="header">
+    //       <nav>
+    //         <ul>
+    //             <li>
+    //                 <button className="login-btn">Log In</button>
+    //             </li>
+    //         </ul>
+    //       </nav>
+    //     </header>
+
+        // <div className="modal">
+        //   <div className="modal-box">
+        //     <div className="modal-body">
+        //         <h3>Log In</h3>
+        //         <form>
+        //             <div className="form-control">
+        //                 <label for="email">Email</label>
+        //                 <input type="email" id="email" required />
+        //             </div>
+        //             <div className="form-control">
+        //                 <label for="password">Password</label>
+        //                 <input type="password" id="password" required />
+        //             </div>
+        //             <button className="btn btn-dark">Log In</button>
+        //         </form>
+        //     </div>
+        //     <div className="modal-footer">
+        //         <p>Don't have an account? <a href="#">Start your free trial</a></p>
+        //     </div>
+        //     <img className="close" src="close.svg" alt="close" />
+        //   </div>
+        // </div>
+//     </div>
+//   )
+// }
+
+// export default Login
